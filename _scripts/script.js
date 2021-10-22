a02fadf62046347fc8a6e9ac010449db5984cd3f
Andy Kirk <andy@andykirk.net>
2021-10-22 16:38:57 +0100

    DO NOT USE

--- _scripts/script.js
+++ _scripts/script.js
@@ -193,6 +193,18 @@ var cookie_html                   =
     var card = function() {
         // Get all elements we want to apply this to:
         var elements = document.querySelectorAll('.js-c-card');
+        
+        var detectLeftButton = function(event) {
+            if (event.metaKey || event.ctrlKey || event.altKey || event.shiftKey) {
+                return false;
+            } else if ('buttons' in event) {
+                return event.buttons === 1;
+            } else if ('which' in event) {
+                return event.which === 1;
+            } else {
+                return (event.button == 1 || event.type == 'click');
+            }
+        }
 
         Array.prototype.forEach.call(elements, function(el, i) {
 
@@ -200,18 +212,28 @@ var cookie_html                   =
 
             var h_link = el.querySelector('.c-card__title > a');
             var cta = el.querySelector('.c-cta');
-            var down, up;
+            var down = false, up = false;
 
             el.addEventListener('mousedown', function(e) {
+                // Detect left click only:
+                var left_click = card.detectLeftButton(e);
+                console.log(left_click);
+                if (!left_click) {
+                    down = false;
+                    return false;
+                }
                 el.classList.add('c-card--is-mousedown');
                 down = +new Date();
             });
 
             el.addEventListener('mouseup', function(e) {
                 el.classList.remove('c-card--is-mousedown');
+                if (!down) {
+                    return;
+                }
                 up = +new Date();
                 if ((up - down) < 200) {
-                    h_link.click();
+                    //h_link.click();
                 }
             });
         });
